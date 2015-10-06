4937fb1d0732d321295da4d0753bb4eac6b35e73
Roope <admin@cxsir.com>
2015-10-06 17:10:24 +0800

    Update Command.php

--- Command.php
+++ Command.php
@@ -1,10 +1,10 @@
 <?php
 require_once dirname(__FILE__) . '/Lib/Alipay.php';
-$alipay = new Alipay(array(
+$alipay = new Alipay([
     'cookie' => '', //支付宝Cookie
     'notify' => '', //通知地址
     'token' => 'please_input_your_token'    //安全密钥
-    ));
+    ]);
 
 while (true) {
     echo date('Y-m-d H:i:s') . "\n";
